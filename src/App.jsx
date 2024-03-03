a57d2ca565d3b5da2ad0254fd2dc3918ed1b697a
vargaae <vargaae@hotmail.com>
2024-03-03 01:17:24 +0100

    101Header setUp folder structure-components-Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Buttton";
+import { Header } from "./components";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello my friend!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Click Me
-        </Button>
+        <Header />
       </div>
-
       <ButtonGradient />
     </>
   );
